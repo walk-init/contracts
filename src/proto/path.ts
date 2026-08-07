ff9acbd70a354e01b451e318e4f6ebf4119e10aa
siberianauka <wildblade207@yandex.ru>
2026-08-07 12:49:25 +0300

    feat: add accounts

--- src/proto/path.ts
+++ src/proto/path.ts
@@ -2,4 +2,5 @@ import { join } from "path";
 
 export const PATH_TO_PROTO_FILES = {
   AUTH: join(__dirname, "../../proto/auth.proto"),
+  ACCOUNT: join(__dirname, "../../proto/account.proto"),
 } as const;
